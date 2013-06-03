da0f943ed233ad50f34bd41f5609e4b488cca5de
John R Live <john@addlocal.com>
2013-06-02 21:06:04 -0400

    Update noderise-setup.sh

--- noderise-setup.sh
+++ noderise-setup.sh
@@ -26,27 +26,24 @@ sudo apt-get install -y libssl-dev
 sudo apt-get install -y git-core
 sudo apt-get install -y curl
 sudo apt-get install -y mc
+sudo apt-get install -y python-software-properties
+sudo apt-get install -y python 
+sudo apt-get install -y g++ 
+sudo apt-get install -y make
 echo '[###### Done ######]'
 sleep 1
 
-echo '[###### Install nodejs v0.10.9 ######]'
+echo '[###### Install nodejs from repo ######]'
 sleep 1
-sudo mkdir /home/tmp
-cd /home/tmp
-sudo wget http://nodejs.org/dist/v0.10.9/node-v0.10.9.tar.gz
-sudo tar xzf node-v0.10.9.tar.gz
-cd node-v0.10.9
-./configure --prefix=/usr
-sudo make install
+sudo add-apt-repository ppa:chris-lea/node.js
+sudo apt-get -y update
+sudo apt-get install -y nodejs
 echo '[###### Done ######]'
 sleep 1
 
 echo '[###### Install NPM ######]'
 sleep 1
-cd /home/tmp
-sudo git clone http://github.com/isaacs/npm.git
-sudo cd npm
-sudo make install
+sudo apt-get install -y npm
 echo '[###### Done ######]'
 sleep 1
 
@@ -65,10 +62,11 @@ sudo chmod 700 ~/apps/startup/app-starter.sh
 echo '{###### Done #####]'
 sleep 1
 
-sudo rm -rf /home/tmp/*
 
 cd ~/
 
+echo 'echo $NODE_PATH'
+echo 'echo $NPM_PATH'
 echo 'Your server has been NodeRISEd... enjoy!'
 
 exit
